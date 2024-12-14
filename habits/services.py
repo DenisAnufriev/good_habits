a0454de6c590cf1d09a934b539ae84ddf1be7cd0
DenisAnufriev <wenomavel89@gmail.com>
2024-12-14 12:05:18 +0300

    update tasks and services

--- habits/services.py
+++ habits/services.py
@@ -2,7 +2,7 @@ import requests
 from django.conf import settings
 
 
-def send_telegram_message(message, tg_chat_id):
+def send_telegram_message(message: str, tg_chat_id: int) -> None:
     """Отправка сообщения в Telegram через бот"""
     url = f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/sendMessage"
     data = {"chat_id": tg_chat_id, "text": message}
