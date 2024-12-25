04098b91d4acb133d5c2432312b9e5e229cd6388
DenisAnufriev <wenomavel89@gmail.com>
2024-12-25 19:51:36 +0300

    update settings.py update services.py

--- habits/services.py
+++ habits/services.py
@@ -1,10 +1,31 @@
 import requests
+from celery import shared_task
 from django.conf import settings
+from requests import RequestException
+import logging
 
+logger = logging.getLogger(__name__)
 
+@shared_task(
+    autoretry_for=(RequestException,),
+    retry_backoff=True,
+    retry_kwargs={'max_retries': 5},
+)
 def send_telegram_message(message: str, tg_chat_id: int) -> None:
-    """Отправка сообщения в Telegram через бот"""
+    """
+    Отправка сообщения в Telegram через бот.
+
+    :param message: Текст сообщения
+    :param tg_chat_id: ID чата в Telegram
+    :raises RequestException: Если отправка сообщения завершилась ошибкой
+    """
     url = f"https://api.telegram.org/bot{settings.TELEGRAM_BOT_TOKEN}/sendMessage"
     data = {"chat_id": tg_chat_id, "text": message}
-    requests.post(url, data=data)
-    print("Отправка сообщения")
+
+    try:
+        response = requests.post(url, data=data, timeout=10)
+        response.raise_for_status()  # Поднимает исключение для статусов >= 400
+        logger.info("Сообщение успешно отправлено в чат %s", tg_chat_id)
+    except RequestException as e:
+        logger.error("Ошибка при отправке сообщения: %s", e)
+        raise
